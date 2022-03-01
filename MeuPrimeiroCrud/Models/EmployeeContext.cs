5bbf1ee6f3059983172d4b59010e4d2acebbec9a
Gabrielgjs <gjsgabriel83@gmail.com>
2022-02-28 23:36:59 -0300

    MeuPrimeiroCrudC#DotNetCore

---
+++ MeuPrimeiroCrud/Models/EmployeeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroCrud.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<Employee> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
